4ea904a6a29dbb0bb5438c259e7455de66ff4ca2
mohsen <mohsen.minavand1384@gmail.com>
2022-01-06 16:36:36 +0330

    add feature that user can get list of birthdays

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ TODO  :
 - [x] create is_valid function 
 - [ ] add feature with can user edit the birthdays 
 - [ ] //     //    //  //  user delete birthdays 
-- [ ] //     //    //  //  user see the list of birthdays 
+- [x] //     //    //  //  user see the list of birthdays 
 - [ ] check the database every day that see is somebody birhtday is coming or not 
--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def is_valid(Birthday) :
 @client.on(events.NewMessage(pattern = '/start')) 
 async def Start(event) :  
     markup = event.client.build_reply_markup([
-        [Button.text('Add Birthday date .')]])
+        [Button.text('Add Birthday date .')] , 
+        [Button.text('list of birthdays .')]])
 
     cursor.execute('SELECT user_id FROM Users')
     output = [user_id[0] for user_id in  cursor] #user_id[0] cause the output executed command is like this (1231235,)
@@ -45,13 +46,19 @@ async def Start(event) :
         cursor.execute(f'INSERT INTO Users (user_id) VALUES(%s)' , (event.message.chat_id)) #if useres started the bot for the first time , we add the user_id to users table 
         await event.respond('Welcome !' , buttons = markup) 
 
+
 @client.on(events.NewMessage(pattern = r'[^/start]'))
 async def Check(event) :
     global Giving_Name , Giving_Birthday , name
     if event.raw_text == 'Add Birthday date .' :
         Giving_Name = 1 
         await client.send_message(event.chat_id , 'What is name to be saved ?') 
-    
+     
+    elif event.raw_text == 'list of birthdays .' : 
+        cursor.execute(f'SELECT * FROM Data WHERE user_id = {event.message.chat_id}') 
+        Birthdays = [f'{i[1]} : {i[2].strftime("%Y/%m/%d")}' for i in cursor]  #i[1] is equal to name and the i[2] is equal to date time and i change the format with strftime
+        await client.send_message(event.chat_id , '\n'.join(Birthdays))        
+
     elif Giving_Name == 1 :
         if event.raw_text.isalpha() :
             Giving_Birthday = 1
